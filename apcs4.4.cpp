fd4378715ab1e97a4238971528a00a3d2de9522a
penguin72487 <agar.io6100@gmail.com>
2020-10-22 23:43:51 +0800

    還要再更小

--- apcs4.4.cpp
+++ apcs4.4.cpp
@@ -2,31 +2,39 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+	int n;
+	int a;
+	vector<int> l;
+	int i,h=0;
+	int m;
+	int o(int m)
+	{
+		
+	}
+
 bool f(int k)
 {
-	int j;
-	printf("fountion %d",k);
-	 return j<k; 
+	int m =o(a);
+	//printf("k m  %d %d\n",k,m);
+	return (k<m); 
 }
 
 int main()
 {
-	int n;
-	int a ;
-	vector<int> l;
 	scanf("%d",&n);
-	int i,h=0;
-	for(i=0;i<2*n;i++)
+			for(i=0;i<2*n;i++)
 	{
 		scanf("%d",&a);
 		l.push_back(a);
 		if(count(l.begin(),l.end(),a)==2)
 		{
-			printf("yes %d",a);
-		h+=count_if(l.begin(),l.end(),f(a))  ;
+			//printf("%d\n",a);
+			o(a);
+		h+=count_if(find(l.begin(),l.end(),a),l.end(),f );
+	}
 	}
 			
-	} 
+	 
 	printf("%d\n",h);
 }
 
