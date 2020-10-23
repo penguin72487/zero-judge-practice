850e4243466b105e157851754e054e5f526e2471
penguin72487 <agar.io6100@gmail.com>
2020-10-23 19:38:56 +0800

    做了一些好玩的東西

--- apcs4.4.cpp
+++ apcs4.4.cpp
@@ -2,31 +2,47 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+	int n;
+	int a;
+	vector<int> l;
+	int i,h=0;
+	int m;
+	int o(int m)
+	{
+		
+	}
 bool f(int k)
 {
-	int j;
-	printf("fountion %d",k);
-	 return j<k; 
+	printf("a %d\n",a);
+	int m =o(a);
+	printf("k m  %d %d\n",k,m);
+	return k<m; 
 }
 
 int main()
 {
-	int n;
-	int a ;
-	vector<int> l;
+
 	scanf("%d",&n);
-	int i,h=0;
-	for(i=0;i<2*n;i++)
-	{
+		int loc[n]={0};
+			for(i=0;i<2*n;i++)
+	{	
 		scanf("%d",&a);
 		l.push_back(a);
+		if(i==0||loc[i]==loc[a])
+		loc[0]=0;
+		else if(loc[a]==0) 
+		loc[a]=i; 
+		
 		if(count(l.begin(),l.end(),a)==2)
 		{
-			printf("yes %d",a);
-		h+=count_if(l.begin(),l.end(),f(a))  ;
+			printf("a %d\n",a);
+		o(a); 
+		h+=count_if(l.begin()+loc[a],l.end(),f);
+			printf("h loc %d\n",h,loc[a]);
+	}
 	}
 			
-	} 
+	 
 	printf("%d\n",h);
 }
 
---
+++ 範圍.1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	unsigned char a;//256 1byt  %hhu %c
+	short int b;//32767  2byt  %hi %hd
+	unsigned short c;//65535 2byt  %hu
+	int d;//21»õ  4byt %i %d
+	unsigned long e;//42»õ 4byt %lu
+	long long int f;//9223¤d¥ü 8byt %lli %lld
+	unsigned long long g;//18446¤d¥ü 8byt %llu
+	 
+	   
+}
