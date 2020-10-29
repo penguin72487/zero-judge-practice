fb2ae34a360d74308107b6f2165432cb97b5e601
penguin72487 <agar.io6100@gmail.com>
2020-10-30 00:10:12 +0800

    123

--- TOI.2.cpp
+++ TOI.2.cpp
@@ -4,30 +4,28 @@
 using namespace std;
 int main()
 {
-	int n;
-	cin>>n;
+	int o;
+	cin>>o;
 	string p;
 	cin>>p;
 	reverse(p.begin(),p.end());
-	int o=p.length();
-	int i,j,l[(p.length()+n-1)/n]={0};
-	int k[(p.length()+n-1)/n];
+	int n=p.length();
+	int i,j,l[(n+n-1)/n]={0};
+	int k[(n+n-1)/n];
 	int m;
 	cout<<p<<endl;
-	for(i=0;i<(p.length()+n-1)/n;i++)
+	for(i=0;i<(n+n-1)/n;i++)
 	{
-		for(j=i*n;j<(i<=p.length()/n)*n+(i>p.length()/n)*(p.length()%n);j++)
+		for(j=i*n;j<n;j++)
 		{
 			l[i]+=p[j]-'0';
 			k[i]=l[i];
 			m=max(m,k[i]);
-			o-=(i+1)*n ;
-			
 		}
 	}
-		for(i=0;i<(p.length()+n-1)/n;i++)
+		for(i=0;i<(n+n-1)/n;i++)
 		cout<<i+1<<" "<<l[i]<<endl;
-		for(i=0;i<(p.length()+n-1)/n;i++)
+		for(i=0;i<(n+n-1)/n;i++)
 		{
 			if(k[i]==m)
 			cout<<i+1<<" "<<l[i]<<endl;
