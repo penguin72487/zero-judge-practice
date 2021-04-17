4b5b9196ac0d60e95c3a7b9857bc1e9c5147ec24
penguin72487 <agar.io6100@gmail.com>
2021-04-17 12:01:23 +0800

    研究排序

---
+++ codeblocks/選擇排序/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void insertion_sort(int list[],int n);
+int main()
+{
+	int n;
+	cin>>n;
+	int list[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>list[i];
+	}
+	insertion_sort(list,n);
+	for(int i=0;i<n;i++)
+	{
+		cout<<list[i]<<" ";
+	}
+	cout<<endl;
+}
+void insertion_sort(int list[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(list[i]>list[j])
+			{
+				list[i]^=list[j]^=list[i]^=list[j];
+			}
+		}
+	}
+
+}
