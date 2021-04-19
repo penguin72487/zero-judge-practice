9e2f335d2c45f68489cfa4f6e9133081061fc90b
penguin72487 <agar.io6100@gmail.com>
2021-04-19 20:26:44 +0800

    拍續排序

--- 排序練習/a233qsort.cpp
+++ 排序練習/a233qsort.cpp
@@ -5,8 +5,13 @@ void qsort(int* arr,int arr_begin,int arr_end);
 int sort_sol(int* arr,int arr_begin,int arr_end);
 int main()
 {
-	int a[] = {10, 7, 8, 9, 1, 5};
-	int n=6;
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
 	qsort(a,0,n-1);
 	for(int i=0;i<n;i++)
 	{
@@ -18,26 +23,26 @@ void qsort(int* arr,int arr_begin,int arr_end)
 	if(arr_begin<arr_end)
 	{
 		int p_index =sort_sol(arr,arr_begin,arr_end);
-		qsort(arr,arr_begin,p_index-1);
-		qsort(arr,p_index+1,arr_end);
+		qsort(arr,arr_begin,p_index+1);
+		qsort(arr,p_index-1,arr_end);
 			
 	}
 	
 }
 int sort_sol(int* arr,int arr_begin,int arr_end)
 {
-		int pivot=arr[arr_end];
-		int i=arr_begin;
+		//int pivot=arr[arr_end];
+		int i=arr_begin-1;
 		for(int j=arr_begin;j<arr_end;j++)
 		{
-			if(arr[j]<=pivot)
+			if(arr[j]<=arr[arr_end])
 			{
-				
-				arr[i]^=arr[j]^=arr[i]^=arr[j];
 				i++;
+				arr[i]^=arr[j]^=arr[i]^=arr[j];
+				
 			}
 		}
-		arr[i]^=arr[arr_end]^=arr[i]^=arr[arr_end];
-		return i;
+		arr[i+1]^=arr[arr_end]^=arr[i+1]^=arr[arr_end];
+		return i+1;
 		
 }
