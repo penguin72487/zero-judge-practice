49924092c5f8b6b32f089894bb2292ea36f56554
penguin72487 <70788551+penguin72487@users.noreply.github.com>
2021-04-21 11:40:06 +0800

    加油

---
+++ f168分配寶物 進階遞迴.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+int n;
+int tre[17];
+int tol=0;
+int avg;
+bool can=0;
+void bfs(int num,int Alice,int Bob,int Cindy);
+int main()
+{
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>tre[i];
+		tol+=tre[i];
+	}
+	if(tol%3==0)
+	{
+		cout<<"NO"<<endl;
+	}
+	else
+	{
+		bfs(0,0,0,0);
+			avg=tol/3;
+		if(can)
+		{
+		
+			cout<<"YES"<<endl;
+		}
+		else
+		{
+			cout<<"NO"<<endl;
+		}
+		
+	}
+	
+}
+void bfs(int num,int Alice,int Bob,int Cindy)
+{
+	if(Alice>avg||Bob>avg||Cindy>avg)
+	{
+		return ;
+	}
+	if(num==n)
+	{
+		if(Alice==Bob&&Bob==Cindy)
+		{
+			can=1;
+			return ;
+		}
+		else
+		{
+			return ;
+		}
+	}
+	if(!can)
+	{
+		bfs(num+1,Alice+tre[num],Bob,Cindy);
+	}
+	if(!can)
+	{
+		bfs(num+1,Alice,Bob+tre[num],Cindy);
+	}
+	if(!can)
+	{
+		bfs(num+1,Alice,Bob,Cindy+tre[num]);
+	}
+	
+}
+
