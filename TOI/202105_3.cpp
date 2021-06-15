6eb345c5f8fc0a04e246590f0533e30c9eff5409
penguin72487 <70788551+penguin72487@users.noreply.github.com>
2021-06-15 09:07:35 +0800

    終於寫完

--- TOI/202105_3.cpp
+++ TOI/202105_3.cpp
@@ -15,14 +15,15 @@ int main()
 		cin>>list[i];
 	}
 	vector<int> ans;
-	for(int i=0;i<n;i++)
+	for(int i=1;i<=n;i++)
 	{
 		
-		for(int j=i;j<n;j++)
+		for(int j=0;j<n-i+1;j++)
 		{ 
 			int tmp=0;
-			for(int k=j;k<n;k++)
+			for(int k=j;k<j+i;k++)
 			tmp+=list[k];
+			//cout<<tmp<<"\n";
 			ans.push_back(tmp);
 		}
 	
