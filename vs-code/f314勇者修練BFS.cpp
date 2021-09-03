5149c6c014e92d32eb8ce2f169a079ad90e3063e
penguin72487 <agar.io6100@gmail.com>
2021-09-03 20:09:46 +0800

    哭阿

--- vs-code/f314勇者修練BFS.cpp
+++ vs-code/f314勇者修練BFS.cpp
@@ -62,45 +62,53 @@ int main()
             matrix[i][j].i_BFSMax = matrix[i][j].i_Data;            
         }
     }
-    bool iib_TrNode[n][m];
-    fill(&iib_TrNode[0][0],&iib_TrNode[n-1][m],0);
-    deque<pair<int, int> *> list;
-    int dx[3] =  {0, 1, 0};
-    int dy[3] = {-1, 0, 1};
+    deque<pair<int, int>*> p_List;
     for (int i = 1; i < m - 1;++i)
     {
         //cout << i << " ";
         pair<int, int>* tmp=new pair<int,int>(1,i);
         //iib_TrNode[1][i] = 1;
         //cout <<"for"<< tmp->first << " " << tmp->second << "\n";
-        list.push_back(tmp);
+        p_List.push_back(tmp);
     }
-    //cout << "\n";
-    while (!list.empty())
+    while(!p_List.empty())
     {
-        pair<int, int> *now = list.front();
-        int x = now->first;
-        int y = now->second;
-        iib_TrNode[x][y] = 1;
-        //cout << x << " " << y << " " << matrix[x][y].i_BFSMax << " ||";
-        for (int i = 0; i < 3; ++i)
+        bool iib_TrNode[n][m];
+        fill(&iib_TrNode[0][0],&iib_TrNode[n-1][m],0);
+        deque<pair<int, int> *> list;
+        int dx[3] =  {0, 1, 0};
+        int dy[3] = {-1, 0, 1};
+        list.push_back(p_List.front());
+        p_List.pop_front();
+        //cout << "\n";
+        while (!list.empty())
         {
-            if (matrix[x + dx[i]][y + dy[i]].i_Data > -2147483647&&iib_TrNode[x+dx[i]][y+dy[i]]==0)
+            pair<int, int> *now = list.front();
+            int x = now->first;
+            int y = now->second;
+            iib_TrNode[x][y] = 1;
+            cout << x << " " << y << " " << matrix[x][y].i_BFSMax << "\n";
+            for (int i = 0; i < 3; ++i)
             {
-                pair<int, int>* tmp= new pair<int,int>(x + dx[i], y + dy[i]);
-                list.push_back(tmp);
-                //cout << "matrix[x + dx[i]][y + dy[i]].i_BFSMax" << matrix[x + dx[i]][y + dy[i]].i_BFSMax << "\n";
-                matrix[x + dx[i]][y + dy[i]].i_BFSMax = max(matrix[x + dx[i]][y + dy[i]].i_BFSMax, matrix[x][y].i_BFSMax + matrix[x + dx[i]][y + dy[i]].i_Data);
+                if (matrix[x + dx[i]][y + dy[i]].i_Data > -2147483647&&iib_TrNode[x+dx[i]][y+dy[i]]==0)
+                {
+                    pair<int, int>* tmp= new pair<int,int>(x + dx[i], y + dy[i]);
+                    list.push_back(tmp);
+                    //cout << "matrix[x + dx[i]][y + dy[i]].i_BFSMax" << matrix[x + dx[i]][y + dy[i]].i_BFSMax << "\n";
+                    matrix[x + dx[i]][y + dy[i]].i_BFSMax = max(matrix[x + dx[i]][y + dy[i]].i_BFSMax, matrix[x][y].i_BFSMax + matrix[x + dx[i]][y + dy[i]].i_Data);
+                }
             }
+            delete now;
+            list.pop_front();
+            
         }
-        delete now;
-        list.pop_front();
+        int i_Max=-2147483648;
+            for (int i = 1; i < m-1;++i)
+            {
+                i_Max = max(i_Max, matrix[n - 2][i].i_BFSMax);
+            }
+            cout <<"ans="<< i_Max << "\n";
     }
-    int i_Max=-2147483648;
-    for (int i = 1; i < m-1;++i)
-    {
-        i_Max = max(i_Max, matrix[n - 2][i].i_BFSMax);
-    }
-    cout << i_Max << "\n";
+   
 
 }
