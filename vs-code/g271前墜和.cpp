083908607be7d69ba8212c26c2bc2f628d61b59a
penguin_72487 <agar.io6100@gmail.com>
2021-09-12 15:14:21 +0800

    擊敗老師

---
+++ vs-code/g271前墜和.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<iterator>
+#include<limits>
+using namespace std;
+unordered_map<int *, int> pi_Map;
+int* min_Ptr(int *val,int *ed)
+{
+    int i_Min=numeric_limits<int>::max();
+    for (auto it = val; it != ed; ++it)
+    {
+        i_Min = min(i_Min, *it);
+    }
+    auto it = val;
+    while(*it!=i_Min)
+    {
+        ++it;
+    }
+    return it;
+}
+int i_Liky(int *val,int *ed)
+{
+    for (auto it = val; it != ed;++it)
+    {
+        cout << *it << " ";
+    }
+    cout << "\n";
+    int *p_Min = min_Ptr(val, ed);
+    if(val+1==ed)
+    {
+        return *val;
+    }
+    cout <<"prior" << pi_Map[p_Min - 1] << " "<< pi_Map[ed - 1] - pi_Map[p_Min] << "\n";
+    if(pi_Map[p_Min-1]>pi_Map[ed-1]-pi_Map[p_Min])
+    {
+        return i_Liky(val, p_Min);
+    }
+    else
+    {
+        return i_Liky(p_Min + 1, ed);
+    }
+}
+int main()
+{
+    cin.tie(0)->sync_with_stdio (0);
+    int n;
+    cin >> n;
+    int list[n];
+    int i_Sum = 0;
+    for(int i = 0; i < n;i_Sum+=list[i],++i)
+    {
+        cin >> list[i];
+        i_Sum += list[i];
+        pi_Map[&list[i]] = i_Sum;
+    }
+        cout<< i_Liky(list,list+n) << "\n";
+}
